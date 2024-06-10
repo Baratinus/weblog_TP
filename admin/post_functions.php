034acca297431e6d83ade8d5501af5130a7836a8
martin <martin.duguey@insa-cvl.fr>
2024-06-10 15:16:25 +0200

    création de post et copie colle des fonctions donner par le proff

--- admin/post_functions.php
+++ admin/post_functions.php
@@ -1,1 +1,66 @@
+/*--------------------
+post_functions.php
+------------------------*/
+// Post variables
+$post_id = 0;
+$isEditingPost = false;
+$published = 0;
+$title = "";
+$post_slug = "";
+$body = "";
+$featured_image = "";
+$post_topic = "";
+/* - - - - - - - - - -
+- Post functions
+- - - - - - - - - - -*/
+// get all posts from WEBLOG DATABSE
+function getAllPosts() {
+    global $conn ;
+    // fonction a besoin de la fonction getPostAuthorById
+    // le code de getPostAuthorById est KDO
+    //TO DO
+    return $posts ;
+}
+function createPost($request_values) {
+    global $conn, $errors, $title, $featured_image, $topic_id, $body, $published;
+}
+    // get the author/username of a post
+function getPostAuthorById($user_id){
+    global $conn ;
+    $sql = « SELECT username FROM users WHERE id=$user_id » ;
+    $result = mysqli_query($conn, $sql) ;
+    if ($result) {
+        // return username
+        return mysqli_fetch_assoc($result)[‘username’] ;
+    } 
+    else {
+        return null ;
+    }
+}
+function editPost($role_id){
+    global $conn, $title, $post_slug, $body, $isEditingPost, $post_id;
+}
+function updatePost($request_values){
+    global $conn, $errors, $post_id, $title, $featured_image, $topic_id, $body, $published;
+}
+// delete blog post
+function deletePost($post_id){
+    global $conn;
+    $sql = "DELETE FROM posts WHERE id=$post_id";
+    if (mysqli_query($conn, $sql)) {
+        $_SESSION['message'] = "Post successfully deleted";
+        header("location: posts.php");
+    exit(0);
+    }
+}
+// toggle blog post : published→unpublished
+function togglePublishPost($post_id, $message){
+    global $conn;
+    $sql = "UPDATE posts SET published=!published WHERE id=$post_id";
+    if (mysqli_query($conn, $sql)) {
+        $_SESSION['message'] = $message;
+        header("location: posts.php");
+        exit(0);
+    }
+}
 
--- admin/posts.php
+++ admin/posts.php
@@ -1,1 +1,76 @@
+<?php include('../config.php'); ?>
+<?php include(ROOT_PATH . '/includes/post_functions.php'); ?>
+<?php include(ROOT_PATH . '/includes/admin/head_section.php'); ?>
 
+<?php
+// Get all admin roles from DB : by admin roles i mean (Admin or Author)
+//$roles = getAdminRoles(); // table roles
+
+// Get all admin users from DB
+//$admins = getAdminUsers(); // by admin roles i mean (Admin or Author), table users
+$posts = getAllPosts();
+?>
+
+<title>Admin | Manage posts</title>
+</head>
+
+<body>
+	<!-- admin navbar -->
+	<?php include(ROOT_PATH . '/includes/admin/header.php') ?>
+	<div class="container content">
+		<!-- Left side menu -->
+		<?php include(ROOT_PATH . '/includes/admin/menu.php') ?>
+
+		<!-- Display records from DB-->
+		<div class="table-div">
+
+			<!-- Display notification message -->
+			<?php include(ROOT_PATH . '/includes/public/messages.php') ?>
+
+			<?php if (empty($posts)) : ?>
+				<h1>No admins in the database.</h1>
+			<?php else : ?>
+				<table class="table">
+					<thead>
+						<th>N</th>
+						<th>Author</th>
+						<th>Title</th>
+                        <th>Views</th>
+						<th colspan="5">Publish</th>
+                        <th colspan="5">Edit</th>
+                        <th colspan="5">Delete</th>
+					</thead>
+					<tbody>
+						<?php foreach ($admins as $key => $admin) : ?>
+							<tr>
+								<td><?php echo $key + 1; ?></td>
+								<td>
+									<?php echo $posts['Author']; ?>, &nbsp;
+									<?php echo $posts['Title']; ?>
+								</td>
+								<td><?php echo $posts['Views']; ?></td>
+                                <td>
+                                    <a class="fa fa-checkmark btn check" href="posts.php?publish-posts=<?php echo $posts['id'] ?>"> 
+                                    </a>
+                                </td>
+								<td>
+									<a class="fa fa-pencil btn edit" href="posts.php?edit-posts=<?php echo $posts['id'] ?>">
+									</a>
+								</td>
+								<td>
+									<a class="fa fa-trash btn delete" href="posts.php?delete-posts=<?php echo $posts['id'] ?>">
+									</a>
+								</td>
+							</tr>
+						<?php endforeach ?>
+					</tbody>
+				</table>
+			<?php endif ?>
+		</div>
+		<!-- // Display records from DB -->
+
+	</div>
+
+</body>
+
+</html>
