b3283aa13698b4ef52b7fc79d9cd4f9576918204
thelio <thelio.doucet@gmail.com>
2024-06-14 11:39:12 +0200

    Création d'un post vraiment cette fois promis !

--- admin/post_functions.php
+++ admin/post_functions.php
@@ -7,6 +7,7 @@ $title = "";
 $post_slug = "";
 $body = "";
 $featured_image = "";
+$file_temp = "";
 $post_topic = "";
 
 /* - - - - - - - - - -
@@ -21,9 +22,9 @@ else if (isset($_POST['update_post'])) {
     updatePost($_POST);
 }
 
-else if (isset($_GET['edit-post'])) {
-    $post_id = $_GET[`edit-post`];
-    editPost($_POST);
+else if (isset($_GET["edit-posts"])) {
+    $post_id = $_GET["edit-posts"];
+    editPost();
 }
 
 /* - - - - - - - - - -
@@ -53,18 +54,19 @@ function getAllPosts() {
 }
 
 function checkFormPost($request_values){
-    global $errors, $title, $featured_image, $topic_id, $body;
+    global $errors, $title, $featured_image, $topic_id, $body, $file_temp;
 
     if (empty($request_values["title"])) {
         array_push($errors, "Title required");
     }else{
         $title = $request_values["title"];
     }
-    $image = $_FILES;
+    $image = $_FILES['featured_image'];
     if (empty($image)) {
         array_push($errors, "Image required");
     } else{
-        $featured_image = $image["featured_image"]['name'];
+        $featured_image = $image['name'];
+        $file_temp = $image['tmp_name'];
     }
     if (empty($request_values["body"])) {
         array_push($errors, "Body required");
@@ -79,23 +81,29 @@ function checkFormPost($request_values){
 }
 
 function createPost($request_values) {
-    global $conn, $errors, $title, $featured_image, $topic_id, $body, $published;
+    global $conn, $errors, $title, $featured_image, $file_temp, $topic_id, $body, $published;
 
     checkFormPost($request_values);
 
     if (empty($errors)) {
         $slug = createSlug($title);
         $currentDate = date("Y-m-d h:i:s");
+        move_uploaded_file($file_temp, ROOT_PATH."/static/images/".$featured_image);
 
-        //getUserId à faire
+        $user_id = getUserId();
 
-        $sql = "INSERT INTO `posts`(`user_id`, `title`, `slug`, `views`, `image`, `body`, `published`, `updated_at`) VALUES (1, '$title', '$slug', 0, '$featured_image', '$body', '$published', '$currentDate');";
+        $sql_post = "INSERT INTO `posts`(`user_id`, `title`, `slug`, `views`, `image`, `body`, `published`, `updated_at`) VALUES ($user_id, '$title', '$slug', 0, '$featured_image', '$body', '$published', '$currentDate');";
 
-        echo $sql;
-        
-    
-        if ($result = mysqli_query($conn, $sql)) {
-            $_SESSION['message'] = "Article created succesfully";
+        if ($result = mysqli_query($conn, $sql_post)) {
+            $post_id = mysqli_insert_id($conn);
+            $new_id = getNewIdPostsTopics();
+            
+            $sql_topic = "INSERT INTO `post_topic`(`id`,`post_id`, `topic_id`) VALUES ($new_id,$post_id,$topic_id);";
+            
+            if ($result = mysqli_query($conn, $sql_topic)) {
+                $_SESSION['message'] = "Article created succesfully";
+            }
+            
         }
 
         header('location: posts.php');
@@ -117,8 +125,8 @@ function getPostAuthorById($user_id){
     }
 }
 
-function editPost($post_id){
-    global $conn, $title, $post_slug, $body, $isEditingPost;
+function editPost(){
+    global $conn, $title, $post_slug, $body, $post_id, $isEditingPost;
 
     $sql = "SELECT `id`, `title`, `slug`, `body` FROM `posts` WHERE id=$post_id;";
 
@@ -141,10 +149,18 @@ function updatePost($request_values){
     checkFormPost($request_values);
 
     if(empty($errors)){
-        $slug = createPost($title);
-        $post_id=$_GET['edit-posts'];
-        $sql = "UPDATE `posts` SET `title`=$title, `featured-image`=$featured_image, `topic_id`=$topic_id, `body=$body WHERE id=$post_id;";
+        $slug = createSlug($title);
+        $sql = "UPDATE `posts` SET `title`=$title, `featured-image`=$featured_image, `topic_id`=$topic_id, `body`=$body WHERE id=$post_id;";
+
+        if ($result = mysqli_query($conn, $sql)) {
+            $_SESSION['message'] = "Article updated succesfully";
+        }
+
+        header('location: posts.php');
+        exit(0);
     }
+
+    
 }
 
 // delete blog post
@@ -166,4 +182,32 @@ function togglePublishPost($post_id, $message){
         header("location: posts.php");
         exit(0);
     }
+}
+
+function getNewIdPostsTopics() {
+    global $conn;
+
+    $sql = "SELECT MAX(`id`) AS max_id FROM `post_topic`;";
+
+    $result = mysqli_query($conn, $sql);
+
+    if ($id = mysqli_fetch_assoc($result)) {
+        return intval($id['max_id']) + 1;
+    } else {
+        return 0;
+    }
+}
+
+function getUserId(){
+    global $conn;
+
+    $user_name = $_SESSION['user']['username'];
+
+    $sql = "SELECT `id` FROM `users` WHERE `username`='$user_name';";
+
+    $result = mysqli_query($conn, $sql);
+
+    $userid = mysqli_fetch_assoc($result);
+
+    return $userid['id'];
 }
